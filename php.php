79f9201c0920677261609de833322b20a22fc09b
maronriku5 <56290905+maronriku5@users.noreply.github.com>
2019-10-24 17:07:03 +0900

    Create php.php

---
+++ php.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="html" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="stylesheet.css">
+    <script type="text/javascript" src="javascript.js"></script>
+    <title>電卓</title>
+  </head>
+  <body>
+    <?php echo "Hello World!\n";
+    echo 7*2;?>
+  </body>
+</html>
